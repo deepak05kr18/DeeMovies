03d63a36b089bcdb29f3c2df1d67fc726dbebf02
deepak05kr18 <122279122+deepak05kr18@users.noreply.github.com>
2023-06-06 17:55:24 +0530

    fixed second error

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import "./style.scss";
-import HeroBanner from './heroBanner/heroBanner';
-import Trending from './trending/trending';
-import Popular from './popular/Popular';
-import TopRated from './topRated/TopRated';
-import Upcoming from './Upcoming/Upcoming';
-Upcoming
-TopRated
-Popular
-HeroBanner
+import React from "react";
 
+import "./style.scss";
+
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
-  return (
-    <div className='homePage'>
-      <HeroBanner />
-      <Trending />
-      <Popular />
-      <TopRated />
-      <Upcoming />
-     
-    </div>
-  )
-}
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
+            <Upcoming />
+        </div>
+    );
+};
 
 export default Home;
